c7e6c6bac27020ad19bac316770d48afaa41f094
Aishwarya <166974321+goaish@users.noreply.github.com>
2024-06-07 16:29:50 +0530

    one.dart

--- async/one.dart
+++ async/one.dart
@@ -1,9 +1,26 @@
-Applications:
-CLI - Command Line Interface
-GUI - Graphical User Interface
+class Account{
+  late int? id;
+  late String? name;
+  late double? blc;
+  show()
+  {
+     print("id : $id , name : $name , blc : $blc");
+  }
+}
+void main()
+{
+  // object
+    // Account account = new Account(); 
+    // Account account = Account(); 
+    var account = Account(); 
+    account..id = 12
+    ..name = "ravi"
+    ..blc = 25000
+    ..show();
 
-->TKINTER - library
-->PYQT5/6 - framework
-
-TKINTER Installation
-Pip install tk
+    var account1 = Account(); 
+    account..id = 13
+    ..name = "ram"
+    ..blc = 95000
+    ..show();
+}
